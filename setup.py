a7e77074439ae5b0b91d37c212a8cb1b1c303926
Rich Temple <rich.otaku@gmail.com>
2017-09-08 11:25:36 -0400

    Initial Setup for newly forked project.

--- setup.py
+++ setup.py
@@ -1,47 +1,39 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
+from __future__ import unicode_literals
 import os
-import sys
+from setuptools import setup, find_packages
 
-from setuptools import setup
 
-import publisher
+def read_file(filename):
+    """Read a file into a string"""
+    path = os.path.abspath(os.path.dirname(__file__))
+    filepath = os.path.join(path, filename)
+    try:
+        return open(filepath).read()
+    except IOError:
+        return ''
 
-version = publisher.__version__
 
-if sys.argv[-1] == 'publish':
-    os.system('python setup.py sdist upload')
-    print("You probably want to also tag the version now:")
-    print("  git tag -a %s -m 'version %s'" % (version, version))
-    print("  git push --tags")
-    sys.exit()
+def get_readme():
+    """Return the README file contents. Supports text,rst, and markdown"""
+    for name in ('README', 'README.rst', 'README.md'):
+        if os.path.exists(name):
+            return read_file(name)
+    return ''
 
-readme = open('README.rst').read()
 
 setup(
     name='django-model-publisher-ai',
-    version=version,
+    version=__import__('publisher').get_version().replace(' ', '-'),
     description="""Handy mixin/abstract class for providing a "publisher workflow" to arbitrary Django models.""",
-    long_description=readme,
-    author='JP74',
-    author_email='opensource@jp74.com',
-    url='https://github.com/andersinno/django-model-publisher-ai',
-    packages=[
-        'publisher',
-    ],
+    long_description=get_readme(),
+    author='Rich Temple',
+    author_email='rtemple@celerity.com',
+    url='https://github.com/natgeosociety/django-model-publisher-ai',
+    packages=find_packages(exclude=['example*', ]),
     include_package_data=True,
-    license="BSD",
-    zip_safe=False,
-    keywords='django-model-publisher',
     classifiers=[
-        'Development Status :: 4 - Beta',
         'Framework :: Django',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
     ],
 )
